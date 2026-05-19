29e72cd3988c2b6872d7080f9110ab30da07a150
shameer125 <alishameer251@gmail.com>
2026-05-18 20:49:43 -0700

    update Reveal.jsx

--- src/components/Reveal.jsx
+++ src/components/Reveal.jsx
@@ -25,7 +25,8 @@ export default function Reveal({ children, className = '' }) {
   return (
     <div
       ref={ref}
-      className={`transition-all duration-700 ease-out motion-reduce:transition-none motion-reduce:translate-y-0 motion-reduce:opacity-100 ${
+      className={`transition-all duration-700 ease-out motion-reduce:transition-none
+      motion-reduce:translate-y-0 motion-reduce:opacity-100 ${
         visible ? 'translate-y-0 opacity-100' : 'translate-y-8 opacity-0'
       } ${className}`}
     >
